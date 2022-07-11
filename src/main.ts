0f0047270972653f66f1fb22e1ea9d80233c39c3
mlatysheva <mlatysheva@gmail.com>
2022-07-11 21:47:45 +0200

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
+import { dirname, join, resolve } from 'path';
 import { AppModule } from './app.module';
+import { parse } from 'yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const rootDirname = dirname(__dirname);
+  const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
+  const document = parse(DOC_API);
+
+  SwaggerModule.setup('doc', app, document);
+  await app.listen(3000);
 }
 bootstrap();
